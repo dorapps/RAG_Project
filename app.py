40e04b8a597b6f1c9be8ea2dc89ed2421fceaef8
david-ordorica <david_ordorica@yahoo.es>
2025-03-15 11:31:02 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,33 @@
+import chainlit as cl
+import requests
+import json
+import pprint
+@cl.on_message
+async def main(message: cl.Message):
+
+        # Send a response back to the user
+    url="http://127.0.0.1:5000/question"
+    myobj = {'question': message.content}
+    try:
+        response = requests.post(url, json=myobj)
+        response.raise_for_status() # Raise an exception for bad status codes (4xx or 5xx)
+
+        # Check the content type to determine how to parse the response
+        content_type = response.headers.get("Content-Type")
+
+        if content_type and "application/json" in content_type:
+            try:
+                data = response.json()
+                pprint.pp(data)
+                await cl.Message(
+                    content=data['respuesta'],    
+                ).send()
+            except json.JSONDecodeError:
+                print("Response was not valid JSON.")
+        else:
+            # Handle non-JSON responses (e.g., plain text)
+            print("Text Response:")
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
